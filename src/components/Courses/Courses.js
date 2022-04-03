7135d560e89930f3020918f7c7f87169d63a70d7
hasibulislam999 <hasib143sl@gmail.com>
2022-04-03 14:47:11 +0600

    first commit

---
+++ src/components/Courses/Courses.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Courses = () => {
+    return (
+        <div>
+            <h1 className='text-3xl'>This is Courses component</h1>
+        </div>
+    );
+};
+
+export default Courses;
